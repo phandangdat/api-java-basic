de598be3a46ba09d65818d5a2a7032683d35416d
Dat Phan <datdangphan@gmail.com>
2024-02-07 11:22:22 +0700

    init project

---
+++ src/main/java/com/controllers/BookController.java
@@ -0,0 +1,20 @@
+package com.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.models.Book;
+
+@RestController
+@RequestMapping(path = "/api/v1/book")
+class BookController {
+    @GetMapping("/listBook")
+    List<Book> getListBook() {
+        return List.of(
+            new Book(10, "Học làm giàu", "Đạt Phan", 1231232131, true)
+        );
+    }
+}
